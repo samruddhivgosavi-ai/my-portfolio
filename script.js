4c9dbc13fd7e676a2d83fb4108cf6d0a88da2b7b
samruddhivgosavi-ai <samruddhi.vgosavi@gmail.com>
2026-02-09 00:22:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the form
+const form = document.getElementById("contactForm");
+
+// Add submit event
+form.addEventListener("submit", function () {
+    alert("Thank you! Your message has been submitted.");
+});
